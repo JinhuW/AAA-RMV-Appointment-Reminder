f6da929c4b2da8bf55ada186d24a7f1b961a5e7e
Jinhu Wang <jinhuwang1127@gmail.com>
2023-02-25 22:53:41 -0500

    remove cache

--- app.py
+++ app.py
@@ -325,6 +325,10 @@ def main():
   #   send_email("AAA reservation notification", message_content, "jinhuwang1127@gmail.com")
   # if sms_token:
   #   sendSMSNotification(f"AAA reservation notification \n {message_content}", ["+18484666289"])
+  if sender_email_pass and sender_email:
+    print(sender_email)
+  if sms_token:
+    print("true")
 
   print("*****************************Done******************************************\n")
 
